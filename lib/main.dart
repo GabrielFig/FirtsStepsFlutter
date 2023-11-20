24603da58d52153cac29d964e9a6f74c36eb48cc
Gabriel Figueroa <jgabriell_00@hotmail.com>
2023-11-20 01:54:05 -0700

    Finish exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_app/presentation/screens/counter_screen.dart';
+import 'package:hello_world_app/presentation/screens/counter_functions_screen.dart';
+// import 'package:hello_world_app/presentation/screens/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: CounterScreen());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.green),
+        home: const CounterFunctionsScreen());
   }
 }
